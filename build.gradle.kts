6adcbd37e59a5ab1f53072e8f6f7998135f2536b
Agung Subastian <agungsubastian1963@gmail.com>
2021-04-01 08:19:28 +0700

    downgrade gradle for appcenter

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("com.android.tools.build:gradle:4.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.31.2-alpha")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.4")
